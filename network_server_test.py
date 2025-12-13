bca0075a0e56e051b17a4dc0d46f7015a8a81dce
IronMan-31 <pranabpandey06@gmail.com>
2025-12-13 13:39:51 +0530

    network fixes done

--- network_server_test.py
+++ network_server_test.py
@@ -1,23 +1,25 @@
 import socket
+import time
 
 s = socket.socket()
 s.connect(("127.0.0.1", 6969))
 
-s.sendall(b"CREATE DATABASE demo")
+s.sendall(b"CREATE DATABASE demo;")  
+
+s.sendall(b"CREATE TABLE demo (")
+time.sleep(0.2)
+
+s.sendall(b"id int")
+time.sleep(0.2)
+
+s.sendall(b");")
 print(s.recv(4096).decode())
 
-s.sendall(b"CREATE TABLE demo (id int);\n")
+# Multiple SQLs in one send
+s.sendall(b"DROP TABLE demo;DROP DATABASE demo;")
+print(s.recv(4096).decode())
 print(s.recv(4096).decode())
 
-s.sendall(b"DROP TABLE demo\n")
-print(s.recv(4096).decode())
-
-s.sendall(b"DROP DATABASE demo")
-print(s.recv(4096).decode())
-
-s.sendall(b"SELECT * FROM students")
-
-while True :  # for testing multi clients
-    pass
-s.sendall(b"exit")
+# Exit
+s.sendall(b"exit;")
 s.close()
