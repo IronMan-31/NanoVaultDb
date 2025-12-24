929244654b42d0a9b0e9a9641986f9a8ff76de67
IronMan-31 <pranabpandey06@gmail.com>
2025-12-24 16:06:56 +0530

    exe files removed and more testing

--- network_server_test.py
+++ network_server_test.py
@@ -8,40 +8,40 @@ def send(sock, cmd):
     resp = sock.recv(8192).decode()
     print(resp)
 
-s = socket.socket()
-s.connect(("127.0.0.1", 6969))
-
-send(s, "CREATE DATABASE vaccumtest;")
-
-send(s, """
-CREATE TABLE StudentRolls (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    roll_no INT NOT NULL
-);
-""")
-
-send(s, "INSERT INTO StudentRolls (roll_no) VALUES (10);")
-send(s, "INSERT INTO StudentRolls (roll_no) VALUES (20);")
-send(s, "INSERT INTO StudentRolls (roll_no) VALUES (30);")
-
-send(s, "SELECT * FROM StudentRolls;")
-
-send(s, 'DELETE FROM StudentRolls WHERE roll_no < "25";')
-
-send(s, "SELECT * FROM StudentRolls;")
-
-s.close()
-# time.sleep(2)
-
 # s = socket.socket()
 # s.connect(("127.0.0.1", 6969))
 
-# # 9️⃣ Use DB again
-# send(s, "USE vaccumtest;")
+# send(s, "CREATE DATABASE vaccumtest;")
 
-# # 🔟 Select after vacuum (physical delete)
+# send(s, """
+# CREATE TABLE StudentRolls (
+#     id INT PRIMARY KEY AUTO_INCREMENT,
+#     roll_no INT NOT NULL
+# );
+# """)
+
+# send(s, "INSERT INTO StudentRolls (roll_no) VALUES (10);")
+# send(s, "INSERT INTO StudentRolls (roll_no) VALUES (20);")
+# send(s, "INSERT INTO StudentRolls (roll_no) VALUES (30);")
+
 # send(s, "SELECT * FROM StudentRolls;")
 
-# # Exit cleanly
-# send(s, "exit;")
+# send(s, 'DELETE FROM StudentRolls WHERE roll_no < "25";')
+
+# send(s, "SELECT * FROM StudentRolls;")
+
 # s.close()
+# # time.sleep(2)
+
+s = socket.socket()
+s.connect(("127.0.0.1", 6969))
+
+# 9️⃣ Use DB again
+send(s, "USE vaccumtest;")
+send(s, "INSERT INTO StudentRolls (roll_no) VALUES (10);")
+# 🔟 Select after vacuum (physical delete)
+send(s, "SELECT * FROM StudentRolls;")
+
+# Exit cleanly
+send(s, "exit;")
+s.close()
