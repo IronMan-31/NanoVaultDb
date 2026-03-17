283fe78b74c14adb74d9e4b0edacfca28948a8fc
shivam <shivam.kumar262200628@gmail.com>
2026-03-18 00:09:35 +0530

    indicator creation happen

--- hft.hpp
+++ hft.hpp
@@ -77,20 +77,21 @@ alignas(64) std::array<TableColumn, MAXHFTSYMBOL> symbolAccessArray;
 
 
 namespace Indicator {
- struct BaseIndicator {
-  double data = 0.0;
-  bool isready = false;
-  alignas(64) std::array<double, HFT::MAXRINGSIZE> ring;
-  alignas(CACHELINE) char name[64];
-  int64_t symbol = -1;
-  int64_t ticksSeen = 0;
+  const int64_t RINGSIZE = 16;
+  struct BaseIndicator {
+    double data = 0.0;
+    uint8_t isready = false;
+    alignas(64) std::array<double,RINGSIZE > ring;
+    alignas(CACHELINE) char name[64];
+    int64_t symbol = -1;
+    int64_t ticksSeen = 0;
 
-  virtual void on_tick(double data) noexcept = 0;
-  virtual const char *getName() const noexcept = 0;
-  virtual int64_t getSymbol() const noexcept = 0;
-  virtual void updateSymBol() noexcept = 0;
-  virtual ~BaseIndicator() = default;
-};
+    virtual void on_tick(double data) noexcept = 0;
+    virtual const char *getName() const noexcept = 0;
+    virtual int64_t getSymbol() const noexcept = 0;
+    virtual void updateSymBol() noexcept = 0;
+    virtual ~BaseIndicator() = default;
+  };
 }; // namespace Indicator
 
 
