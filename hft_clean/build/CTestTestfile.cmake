03f423d3e1419ad1ce8f6c8ea9ee85a77501c5ce
shivam <shivam.kumar262200628@gmail.com>
2026-04-11 13:44:57 +0530

    added binance and orderbook

--- hft_clean/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shivam/Desktop/learning/NanoVaultdp/NanoVaultDb/hft_clean
-# Build directory: /home/shivam/Desktop/learning/NanoVaultdp/NanoVaultDb/hft_clean/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(all_tests "/home/shivam/Desktop/learning/NanoVaultdp/NanoVaultDb/hft_clean/build/run_tests")
-set_tests_properties(all_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/shivam/Desktop/learning/NanoVaultdp/NanoVaultDb/hft_clean/CMakeLists.txt;51;add_test;/home/shivam/Desktop/learning/NanoVaultdp/NanoVaultDb/hft_clean/CMakeLists.txt;0;")
--- hft_clean/build/test_boost.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/asio.hpp>
-#include <boost/beast.hpp>
-#include <boost/beast/ssl.hpp>
-#include <iostream>
-int main() {
-    boost::asio::io_context ioc;
-    std::cout << "Boost ASIO found.\n";
-    return 0;
-}
